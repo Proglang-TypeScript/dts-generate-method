fe594fd080d691d5824f22f7e5ca0acd4824d343
Fernando Cristiani <fer5cristiani@gmail.com>
2020-04-20 21:40:07 +0200

    Add examples for template module

--- 9_compare/dts-compare/examples/module/module.actual.d.ts
+++ 9_compare/dts-compare/examples/module/module.actual.d.ts
@@ -1,1 +1,2 @@
 export function myFunction(a: number): number;
+export function myOtherFunction(a: Array<any>, b: number): number;
--- 9_compare/dts-compare/examples/module/module.expected.d.ts
+++ 9_compare/dts-compare/examples/module/module.expected.d.ts
@@ -1,1 +1,8 @@
 export function myFunction(a: string): number;
+export function myOtherFunction(a: string[], b: A.B): number;
+
+export namespace A {
+	export interface B {
+		a: string[]
+	}
+}
