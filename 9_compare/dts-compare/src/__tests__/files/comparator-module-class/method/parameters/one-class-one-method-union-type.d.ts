0a42eee07f42574a6e81845e8678c796235fb0cc
Fernando Cristiani <fer5cristiani@gmail.com>
2021-02-28 17:28:15 +0100

    Fix lint issues

--- 9_compare/dts-compare/src/__tests__/files/comparator-module-class/method/parameters/one-class-one-method-union-type.d.ts
+++ 9_compare/dts-compare/src/__tests__/files/comparator-module-class/method/parameters/one-class-one-method-union-type.d.ts
@@ -3,6 +3,7 @@ export default OneClass;
 declare class OneClass {
   next(a: string | number): string;
   anotherNext(a: string | number | boolean): string;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   myMethod(b: any, c: 'hello', d: 1 | 2 | 3 | 4 | 5, a?: string): string;
 }
 
