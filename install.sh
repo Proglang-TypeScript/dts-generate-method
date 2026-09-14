4a676721330e15af35f7887cb5bf41d1ccd0520f
Peter Thiemann <peter.thiemann@gmail.com>
2026-09-14 13:07:51 +0200

    Modernize Node24 dependencies and remediate security advisories

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-
-SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
-
-INSTALL_FOLDER=$SCRIPT_PATH/lib
-
-rm -rf $INSTALL_FOLDER
-mkdir $INSTALL_FOLDER
-
-git clone https://github.com/proglang/ts-declaration-file-generator-service.git $INSTALL_FOLDER
-$INSTALL_FOLDER/install.sh
+set -euo pipefail
+SCRIPT_PATH="$(cd "$(dirname "$0")"; pwd -P)"
+INSTALL_FOLDER="$SCRIPT_PATH/lib"
+if [[ -e "$INSTALL_FOLDER" ]]; then
+    BACKUP=$(mktemp -d "$SCRIPT_PATH/.preserved.XXXXXXXX")
+    mv "$INSTALL_FOLDER" "$BACKUP/checkout"
+    echo "Previous service checkout preserved at $BACKUP/checkout"
+fi
+git clone --no-checkout https://github.com/Proglang-TypeScript/ts-declaration-file-generator-service.git "$INSTALL_FOLDER"
+git -C "$INSTALL_FOLDER" checkout --detach 6d6fbb5238f54a2a19354e9e7643e61de60b2e11
+"$INSTALL_FOLDER/install.sh"
